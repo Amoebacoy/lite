3f049d99628e880da1b0c049d236c769afe8608f
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-08-09 01:59:57 +0700

    Update lite.sh

--- lite.sh
+++ lite.sh
@@ -59,6 +59,13 @@ mkdir /var/lib/crot;
 mkdir /etc/xray;
 echo "IP=" >> /var/lib/crot/ipvps.conf
 echo "IP=" >> /var/lib/akbarstorevpn/ipvps.conf
+cd /etc/
+cat > /etc/hosts <<-END
+185.199.108.133 raw.githubusercontent.com
+185.199.109.133 raw.githubusercontent.com
+185.199.110.133 raw.githubusercontent.com
+185.199.111.133 raw.githubusercontent.com
+END
 cd
 #
 # Add Domain
